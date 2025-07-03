b3a9e34d5d989d624aa9dcfe66693cade31fc580
Bill Mamorno <mamornobillc@gmail.com>
2025-07-03 17:21:30 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $database = new mysqli("localhost", "root", "root", "edoc");
+    $database = new mysqli("localhost", "root", "", "edoc");
     if ($database->connect_error) {
         die("Connection failed: " . $database->connect_error);
     }
