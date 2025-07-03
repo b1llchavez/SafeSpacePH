3665b3cca218e1670a3e98be888227f88f8e0da2
gwenmorales <202411345@fit.edu.ph>
2025-07-03 15:29:13 +0800

    Delete connection.php

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $database = new mysqli("localhost", "root", "root", "edoc");
-    if ($database->connect_error) {
-        die("Connection failed: " . $database->connect_error);
-    }
-?>
