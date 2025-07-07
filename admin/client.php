b2a30d388bd9e67418cad6295bd28acfc0f520e1
Bill Mamorno <mamornobillc@gmail.com>
2025-07-07 14:45:41 +0800

    fixed

--- admin/client.php
+++ admin/client.php
@@ -242,29 +242,30 @@
                                     $tel=$row["ctel"];
                                     
                                     echo '<tr>
-                                        <td> &nbsp;'.
-                                        substr($name,0,35)
-                                        .'</td>
-                                        <td>
-                                        '.substr($nic,0,12).'
-                                        </td>
-                                        <td>
-                                            '.substr($tel,0,10).'
-                                        </td>
-                                        <td>
-                                        '.substr($email,0,20).'
-                                         </td>
-                                        <td>
-                                        '.substr($dob,0,10).'
-                                        </td>
-                                        <td >
-                                        <div style="display:flex;justify-content: center;">
-                                        
-                                        <a href="?action=view&id='.$cid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
-                                       
-                                        </div>
-                                        </td>
-                                    </tr>';
+    <td> &nbsp;'.
+
+    substr($name ?? '',0,35)
+    .'</td>
+    <td>
+    '.substr($nic ?? '',0,12).'
+    </td>
+    <td>
+        '.substr($tel ?? '',0,10).'
+    </td>
+    <td>
+    '.substr($email ?? '',0,20).'
+     </td>
+    <td>
+    '.substr($dob ?? '',0,10).'
+    </td>
+    <td >
+    <div style="display:flex;justify-content: center;">
+    
+    <a href="?action=view&id='.$cid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+   
+    </div>
+    </td>
+</tr>';
                                     
                                 }
                             }
