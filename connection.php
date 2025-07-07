75b663e58eb7d3dbf7b11c190348870bf6889dd3
gwenmorales <202411345@fit.edu.ph>
2025-07-07 22:02:04 +0800

    #	modified:   safespace.sql

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","root","SafeSpacePH");
+    $database= new mysqli("localhost","root","","SafeSpacePH");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
