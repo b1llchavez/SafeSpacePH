65f5e076376d26e422b1735dd483caaa894fd11b
Bill Mamorno <mamornobillc@gmail.com>
2025-07-27 02:19:20 +0800

    updated sql for default accs

--- SafeSpacePH.sql
+++ SafeSpacePH.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 25, 2025 at 10:40 AM
+-- Generation Time: Jul 26, 2025 at 06:17 PM
 -- Server version: 8.0.40
 -- PHP Version: 8.3.14
 
@@ -57,14 +57,6 @@ CREATE TABLE `appointment` (
   `cancellation_explanation` text
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `appointment`
---
-
-INSERT INTO `appointment` (`appoid`, `cid`, `apponum`, `scheduleid`, `appodate`, `status`, `description`, `cancellation_reason`, `cancellation_explanation`) VALUES
-(17, 13, 1753340073, 27, '2025-07-24', 'cancelled', 'HAHAUHDAUIGDAHISBDANSK D', 'Personal emergency', 'testing ulit'),
-(20, 13, 1753428948, 31, '2025-07-25', 'pending', 'WORKING SHA', NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -86,8 +78,7 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`cid`, `cemail`, `cname`, `cpassword`, `caddress`, `cdob`, `ctel`) VALUES
-(15, 'm4mornob1ll@gmail.com', 'First Name Last Name', 'Testing123!', 'Address ni First Name, Last Name', '2001-01-01', '9917912370'),
-(13, 'mamornobillc@gmail.com', 'Bill Mamorno', '123', '119 San Isidro St., Villa Espana II, Brgy. Tatalon', '2006-03-18', '9917912372');
+(1, 'client@safespaceph.com', 'Client User', '123', '123 Client Address, Quezon City', '2000-01-01', '09123456789');
 
 -- --------------------------------------------------------
 
@@ -123,13 +114,6 @@ CREATE TABLE `identity_verifications` (
   `is_verified` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `identity_verifications`
---
-
-INSERT INTO `identity_verifications` (`id`, `first_name`, `middle_name`, `last_name`, `suffix`, `dob`, `sex`, `civil_status`, `citizenship`, `birth_place`, `present_address`, `permanent_address`, `email`, `contact_number`, `emergency_contact_name`, `emergency_contact_number`, `emergency_contact_relationship`, `id_type`, `id_number`, `id_photo_front_path`, `id_photo_back_path`, `profile_photo_path`, `agree_terms`, `submission_date`, `is_verified`) VALUES
-(3, 'Bill', 'Chavez', 'Mamorno', '', '2006-03-18', 'Male', 'Single', 'Filipino', 'Manila', '119 San Isidro St., Villa Espana II, Brgy. Tatalon', '119 San Isidro St., Villa Espana II, Brgy. Tatalon', 'mamornobillc@gmail.com', '09917912370', 'Edmuna Mamorno', '09396035648', 'Mother', 'Driver&#039;s License', '11001212154548', 'uploads/id_front/front_6873c9f225cc6_id 1.jpg', 'uploads/id_back/back_6873c9f225fd7_id 2.png', 'uploads/profile_photo_client/profile_6873c9f226122_profile photo sample.jpg', 1, '2025-07-13 15:00:02', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -153,7 +137,7 @@ CREATE TABLE `lawyer` (
 --
 
 INSERT INTO `lawyer` (`lawyerid`, `lawyeremail`, `lawyername`, `lawyerpassword`, `lawyerrollid`, `lawyertel`, `specialties`, `meeting_link`, `meeting_platform`) VALUES
-(1, 'lawyer@safespaceph.com', 'Test Lawyer', '123', '202411951', '09917912370', 18, 'SafeSpace PH Office, P. Paredes St., Sampaloc, Manila 1015', 'SafeSpace PH Office');
+(1, 'lawyer@safespaceph.com', 'Test Lawyer', '123', '12345', '09123456780', 1, 'SafeSpace PH Office, P. Paredes St., Sampaloc, Manila 1015', 'SafeSpace PH Office');
 
 -- --------------------------------------------------------
 
@@ -178,13 +162,6 @@ CREATE TABLE `reports` (
   `admin_notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `reports`
---
-
-INSERT INTO `reports` (`id`, `client_id`, `reporter_name`, `reporter_phone`, `reporter_email`, `title`, `description`, `legal_consultation_requested`, `supplementary_notes`, `file_name`, `file_path`, `uploaded_at`, `report_status`, `admin_notes`) VALUES
-(6, 13, 'Bill Mamorno', '09917912370', 'mamornobillc@gmail.com', 'Violation Report: Public Harassment', 'Violation Type: Public Harassment\nDate of Incident: 2005-11-11\nTime of Incident: 11:11\nLocation of Incident: working\nPerpetrator Information: white haired\nVictim\'s Name: Bill ERIC Mamorno\nVictim\'s Contact: mamornobillc@gmail.com\n\n---Reporter\'s Detailed Description---\nworking 2', 'Yes', 'sdsdsds', 'report_68821a0c41eea.jpg', '../uploads/reports/report_68821a0c41eea.jpg', '2025-07-24 19:33:32', 'rejected', 'sasa');
-
 -- --------------------------------------------------------
 
 --
@@ -199,19 +176,7 @@ CREATE TABLE `schedule` (
   `scheduletime` time DEFAULT NULL,
   `nop` int DEFAULT NULL,
   `clientid` int NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `schedule`
---
-
-INSERT INTO `schedule` (`scheduleid`, `lawyerid`, `title`, `scheduledate`, `scheduletime`, `nop`) VALUES
-(26, NULL, 'DDD', '2026-02-11', '15:33:00', 1),
-(27, '1', 'TETSING TESTING', '2027-11-11', '11:11:00', 1),
-(28, '1', 'TESTING JULY 24 (2)', '2026-02-22', '12:32:00', 2),
-(29, NULL, 'IT IS WORKING', '2025-07-31', '11:11:00', NULL),
-(30, '1', 'ETO WORKING NA DAPAT', '2025-07-31', '23:59:00', NULL),
-(31, NULL, 'TESTING ULIT 33P5PM', '2026-11-11', '11:59:00', NULL);
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -289,13 +254,6 @@ CREATE TABLE `volunteer_lawyer` (
   `is_verified` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
---
--- Dumping data for table `volunteer_lawyer`
---
-
-INSERT INTO `volunteer_lawyer` (`id`, `last_name`, `first_name`, `email`, `contact_number`, `home_address`, `years_experience`, `roll_number`, `license_file`, `profile_photo`, `motivation`, `consent_background_check`, `agree_terms`, `info_certified`, `submitted_at`, `availability_hours`, `urgent_consult`, `commitment_months`, `preferred_areas`, `bar_region`, `resume_file`, `affiliation`, `reference_contact`, `is_verified`) VALUES
-(2, 'Doroja', 'Gerard', 'billchavezmamorno075@gmail.com', '09917912370', '119 San Isidro St., Villa Espana II, Brgy. Tatalon', 4, '202411951', 'uploads/license/license_68835ece357b3.jpeg', 'uploads/profile_photo_lawyer/photo_68835ece35b12.jpeg', 'i wanted to because i want to', 1, 1, 1, '2025-07-25 10:39:10', 5, 'Yes', 12, 'Family Law', 'Metro Manila', 'uploads/resume/resume_68835ece366bd.pdf', 'Partner', '4', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -314,9 +272,7 @@ CREATE TABLE `webuser` (
 INSERT INTO `webuser` (`email`, `usertype`) VALUES
 ('admin@safespaceph.com', 'a'),
 ('lawyer@safespaceph.com', 'l'),
-('m4mornob1ll@gmail.com', 'u'),
-('mamornobillc@gmail.com', 'u'),
-('jarix90822@mvpmedix.com', 'l');
+('client@safespaceph.com', 'c');
 
 --
 -- Indexes for dumped tables
@@ -395,43 +351,43 @@ ALTER TABLE `webuser`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appoid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `appoid` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `identity_verifications`
 --
 ALTER TABLE `identity_verifications`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `lawyer`
 --
 ALTER TABLE `lawyer`
-  MODIFY `lawyerid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `lawyerid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `scheduleid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `scheduleid` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `volunteer_lawyer`
 --
 ALTER TABLE `volunteer_lawyer`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
